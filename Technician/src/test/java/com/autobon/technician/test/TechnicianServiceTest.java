36930d6de52e2ea06c96992265cfa039b4b42ed7
黄大超 <dchuang@incardata.com.cn>
2016-02-18 11:20:20 +0800

    添加一个专用于测试的token

--- Technician/src/test/java/com/autobon/technician/test/TechnicianServiceTest.java
+++ Technician/src/test/java/com/autobon/technician/test/TechnicianServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.transaction.Transactional;
 
