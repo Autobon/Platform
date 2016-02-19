32aef9422e462757c021618eafb4776a985aa25d
ZaiHang <wnddt0717@163.com>
2016-02-19 17:03:14 +0800

    colunm comment and gps tool

--- src/main/resources/db/migration/V20160217.2__codeitem.sql
+++ src/main/resources/db/migration/V20160217.2__codeitem.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS sys_codeitem (
   id int(11) NOT NULL,
-  code varchar(32) NOT NULL,
-  codemap varchar(32) NOT NULL,
-  name varchar(32) NOT NULL,
-  remark varchar(100) DEFAULT NULL,
+  code varchar(32) NOT NULL COMMENT '字典选项code',
+  codemap varchar(32) NOT NULL COMMENT '字典code',
+  name varchar(32) NOT NULL COMMENT '字典选项名称',
+  remark varchar(100) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (id)
 ) COMMENT='字典item表';
