05f812a3bcc66ab1e2fbd647e703e078c7e21461
黄大超 <dchuang@incardata.com.cn>
2016-02-19 21:22:08 +0800

    .

--- src/main/java/com/autobon/platform/controller/AccountController.java
+++ src/main/java/com/autobon/platform/controller/AccountController.java
@@ -30,7 +30,7 @@ public class AccountController {
     private String env;
 
     /***
-     * 在提交含有验证码图片的表单时,为了对验证码正确性进行验证,请一并传入获取验证码图片时的seed参数
+     * 在提交含有验证码图片的表单时,为了对验证码正确性进行验证,请一并传入获取验证码图片时的seed参数.为了不使用sessionId的折衷方法.
      * @param seed 由客户端生成的用于标识自己的随机字符串,建议使用如下形式: 代表时间戳长整数 + ':' + 12位随机码
      * @param out
      * @throws IOException
