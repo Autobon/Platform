e3762e20552310a04ee351a125818e7dfafbf2da
黄大超 <dchuang@incardata.com.cn>
2016-02-22 15:54:27 +0800

    更新redis配置

--- src/main/java/com/autobon/platform/config/RedisCacheConfig.java
+++ src/main/java/com/autobon/platform/config/RedisCacheConfig.java
@@ -1,15 +1,11 @@
 package com.autobon.platform.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * Created by dave on 16/2/17.
@@ -38,19 +34,19 @@ public class RedisCacheConfig extends CachingConfigurerSupport {
         return redisConnectionFactory;
     }
 
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(cf);
-        return redisTemplate;
-    }
+//    @Bean
+//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+//        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(cf);
+//        return redisTemplate;
+//    }
 
-    @Bean
-    public CacheManager cacheManager(RedisTemplate redisTemplate) {
-        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
-
-        // Number of seconds before expiration. Defaults to unlimited (0)
-        cacheManager.setDefaultExpiration(3000); // Sets the default expire time (in seconds)
-        return cacheManager;
-    }
+//    @Bean
+//    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+//        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+//
+//        // Number of seconds before expiration. Defaults to unlimited (0)
+//        cacheManager.setDefaultExpiration(3000); // Sets the default expire time (in seconds)
+//        return cacheManager;
+//    }
 }
