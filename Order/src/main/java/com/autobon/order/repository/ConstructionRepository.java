3073b966b3e2a814cdd6a0888c0a31b86f7a2fca
ZaiHang <wnddt0717@163.com>
2016-02-23 16:17:35 +0800

    signIn method

---
+++ Order/src/main/java/com/autobon/order/repository/ConstructionRepository.java
@@ -0,0 +1,12 @@
+package com.autobon.order.repository;
+
+import com.autobon.order.entity.Construction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by yuh on 2016/2/23.
+ */
+@Repository
+public interface ConstructionRepository extends JpaRepository<Construction, Integer> {
+}
--- src/main/java/com/autobon/platform/controller/order/OrderController.java
+++ src/main/java/com/autobon/platform/controller/order/OrderController.java
@@ -1,6 +1,8 @@
 package com.autobon.platform.controller.order;
 
+import com.autobon.order.entity.Construction;
 import com.autobon.order.entity.Order;
+import com.autobon.order.repository.ConstructionRepository;
 import com.autobon.order.service.OrderService;
 import com.autobon.platform.utils.JsonMessage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,6 +22,9 @@ import java.util.List;
 public class OrderController {
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private ConstructionRepository constructionRepository;
 
     @RequestMapping(value = "/order/orderList", method = RequestMethod.GET)
     public JsonMessage orderList() throws Exception{
@@ -37,6 +43,23 @@ public class OrderController {
 
         jsonMessage.setData(order);
         return  jsonMessage;
+    }
+
+    @RequestMapping(value = "/order/signIn", method = RequestMethod.POST)
+    public JsonMessage signIn(
+            @RequestParam("rtpositionLon") String rtpositionLon,
+            @RequestParam("rtpositionLat") String rtpositionLat,
+            @RequestParam("technicianId") int technicianId,
+            @RequestParam("orderId") int orderId){
+        JsonMessage jsonMessage = new JsonMessage(true,"signIn");
+        Construction construction = new Construction();
+        construction.setOrderId(orderId);
+        construction.setTechnicianId(technicianId);
+        construction.setRtpositionLon(rtpositionLon);
+        construction.setRtpositionLat(rtpositionLat);
+        construction.setSigninTime(new Date());
+        constructionRepository.save(construction);
+        return jsonMessage;
     }
 
 }
