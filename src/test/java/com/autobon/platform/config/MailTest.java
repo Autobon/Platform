47928097e1ec1abbde20ec03b2b95999824f7983
ZaiHang <wnddt0717@163.com>
2016-03-03 16:56:07 +0800

    from and to  username

--- src/test/java/com/autobon/platform/config/MailTest.java
+++ src/test/java/com/autobon/platform/config/MailTest.java
@@ -1,8 +1,9 @@
-package com.autobon.platform.admin;
+package com.autobon.platform.config;
 
 import com.autobon.platform.MvcTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
@@ -18,12 +19,15 @@ public class MailTest extends MvcTest{
     @Autowired
     JavaMailSender mailSender;
 
+    @Value("${com.autobon.mail.username}")
+    private String username;
+
     @Test
     public void testSendMail() throws MessagingException {
         MimeMessage msg = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(msg, true, "UTF-8");
-        helper.setFrom("339445148@qq.com");
-        helper.setTo("339445148@qq.com");
+        helper.setFrom(username);
+        helper.setTo(username);
         helper.setSubject("测试邮件");
         helper.setText("<h3>就是一个测试邮件！</h3>", true);
         mailSender.send(msg);
