9652238de4f75c3f1d0d782381bbc230a50e0eef
黄大超 <dchuang@incardata.com.cn>
2016-03-07 13:58:34 +0800

    清理文件及注掉的代码

--- Order/src/main/resources/db/migration/V20160219.3__t_evaluate.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `t_evaluate` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_id` int(11) DEFAULT NULL COMMENT '订单id',
-  `star_level` int(1) DEFAULT NULL COMMENT '评价星个数',
-  `check_options` varchar(100) DEFAULT NULL COMMENT '勾选项(逗号隔开)',
-  `advice` varchar(255) DEFAULT NULL COMMENT '评价意见和建议',
-  PRIMARY KEY (`Id`),
-  KEY `order_id` (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单评价表';
