932b390847543e23f57c04b229a52330c014d770
黄大超 <dchuang@incardata.com.cn>
2016-03-11 10:11:40 +0800

    .

--- src/main/java/com/autobon/platform/config/WebConfig.java
+++ src/main/java/com/autobon/platform/config/WebConfig.java
@@ -1,9 +1,7 @@
 package com.autobon.platform.config;
 
-import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -14,10 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
-    @Bean
-    public SessionFactory getHibernateSession() {
-        return new LocalSessionFactoryBean().getObject();
-    }
 
     @Bean
     public MultipartResolver multipartResolver() {
