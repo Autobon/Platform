b5f7333d063aa33d81d2ea495d7ce2017b337f88
黄大超 <dchuang@incardata.com.cn>
2016-03-13 00:29:19 +0800

    添加对Angularjs中html5mode的支持

--- src/main/java/com/autobon/platform/config/WebConfig.java
+++ src/main/java/com/autobon/platform/config/WebConfig.java
@@ -1,17 +1,25 @@
 package com.autobon.platform.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 /**
  * Created by dave on 16/2/18.
  */
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+    private static final Logger log = LoggerFactory.getLogger(WebConfig.class);
 
     @Bean
     public MultipartResolver multipartResolver() {
@@ -25,6 +33,18 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("/", "classpath:/static/");
+                .addResourceLocations("/", "classpath:/static/")
+                .setCachePeriod(365*24*3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        if (location.exists() && location.isReadable()) {
+                            return location;
+                        } else {
+                            return new ClassPathResource("/static/index.html");
+                        }
+                    }
+                });
     }
 }
