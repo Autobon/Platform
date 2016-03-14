b92ea15c8cbe35328ea7ce2132bee7ba4a4f0274
黄大超 <dchuang@incardata.com.cn>
2016-03-14 20:20:32 +0800

    空提交，重新跑一次

--- Technician/src/main/resources/db/migration/V20160315.2__v_technician.sql
+++ Technician/src/main/resources/db/migration/V20160315.2__v_technician.sql
@@ -1,2 +1,3 @@
 CREATE VIEW `v_technician` AS select t.*, s.star_rate, s.balance, s.unpaid_orders, s.total_orders, s.comment_count
 from t_technician t left join t_tech_stat s on t.id = s.tech_id;
+
