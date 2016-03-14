2632a90699ca76207ad19edcc5388ba56a41fc52
ZaiHang <wnddt0717@163.com>
2016-03-14 14:42:56 +0800

    后台对合作商户的查询条件修改，后台对合作商户的审核

---
+++ Cooperators/src/main/java/com/autobon/cooperators/service/ReviewCooperService.java
@@ -0,0 +1,20 @@
+package com.autobon.cooperators.service;
+
+import com.autobon.cooperators.entity.ReviewCooper;
+import com.autobon.cooperators.repository.ReviewCooperRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by yuh on 2016/3/14.
+ */
+@Service
+public class ReviewCooperService {
+    @Autowired
+    private ReviewCooperRepository reviewCooperRepository;
+
+    public void save(ReviewCooper reviewCooper) {
+        reviewCooperRepository.save(reviewCooper);
+    }
+
+}
