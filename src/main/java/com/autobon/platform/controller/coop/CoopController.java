798965532c1ce0fdaa473cee8825bc00d2d9f9f2
ZaiHang <wnddt0717@163.com>
2016-03-15 16:50:35 +0800

    合作商户加盟

---
+++ src/main/java/com/autobon/platform/controller/coop/CoopController.java
@@ -0,0 +1,7 @@
+package com.autobon.platform.controller.coop;
+
+/**
+ * Created by yuh on 2016/3/15.
+ */
+public class CoopController {
+}
---
+++ src/test/java/com/autobon/platform/coop/CoopControllerTest.java
@@ -0,0 +1,7 @@
+package com.autobon.platform.coop;
+
+/**
+ * Created by yuh on 2016/3/15.
+ */
+public class CoopControllerTest {
+}
