f8d90c8a315c42fb77da746a93a4565063e786a5
dchuangincar <dchuang@incardata.com.cn>
2016-03-31 10:39:07 +0800

    .

--- src/main/java/com/autobon/platform/controller/admin/BillController.java
+++ src/main/java/com/autobon/platform/controller/admin/BillController.java
@@ -1,11 +1,20 @@
 package com.autobon.platform.controller.admin;
 
+import com.autobon.shared.JsonMessage;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by dave on 16/3/31.
  */
 @RestController("adminBillController")
+@RequestMapping("/api/web/admin/bill")
 public class BillController {
 
+    @RequestMapping(method = RequestMethod.GET)
+    public JsonMessage search() {
+
+        return new JsonMessage(true);
+    }
 }
