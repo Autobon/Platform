6b995286640c1e54fd1ecaeee979e6721b683a6c
dchuangincar <dchuang@incardata.com.cn>
2016-03-31 10:23:57 +0800

    .

---
+++ src/main/java/com/autobon/platform/controller/admin/BillController.java
@@ -0,0 +1,11 @@
+package com.autobon.platform.controller.admin;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dave on 16/3/31.
+ */
+@RestController("adminBillController")
+public class BillController {
+
+}
