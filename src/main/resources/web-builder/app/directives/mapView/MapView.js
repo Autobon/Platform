7b6720d92125da76237b548b957df9726bd16160
dchuangincar <dchuang@incardata.com.cn>
2016-04-06 18:25:34 +0800

    MapView  directive

--- src/main/resources/web-builder/app/directives/mapView/MapView.js
+++ src/main/resources/web-builder/app/directives/mapView/MapView.js
@@ -1,8 +1,67 @@
 import {Injector} from 'ngES6';
+import angular from 'angular';
 import $ from 'jquery';
 
 export default class MapView extends Injector {
-    static $inject = [];
+    static $inject = ['$timeout'];
+    static MapViewOverlay = class extends window.BMap.Overlay {
+        constructor(object, onmouseover, onclick) {
+            super();
+            this.point       = new window.BMap.Point(object.lng, object.lat);
+            this.label       = object.label;
+            this.data        = object;
+            this.onmouseover = onmouseover;
+            this.onclick     = onclick;
+        }
+
+        initialize(map) {
+            this.map = map;
+            let div  = this.div = $(`<div><span>${this.label}</span><div class="arrow"></div></div>`);
+            div.css({
+                position: 'absolute',
+                border: '1px solid #BC3B3A',
+                color: '#FFF',
+                padding: '2px',
+                'line-height': '18px',
+                'white-space': 'nowrap',
+                'font-size': '14px',
+                'background-color': '#EE5D5B',
+            });
+            $('.arrow', div).css({
+                background: 'url(http://map.baidu.com/fwmap/upload/r/map/fwmap/static/house/images/label.png) no-repeat',
+                position: 'absolute',
+                width: '11px',
+                height: '10px',
+                top: '22px',
+                left: '10px',
+                overflow: 'hidden',
+            });
+            if (this.onmouseover) {
+                div.mouseover(this.onmouseover);
+            }
+            if (this.onclick) {
+                div.click(this.onclick);
+            }
+            map.getPanes().markerPane.appendChild(div[0]);
+            return div[0];
+        }
+
+        draw() {
+            let pixel = this.map.pointToOverlayPixel(this.point);
+            this.div.css({
+                left: pixel.x - 10,
+                top : pixel.y - 30,
+            });
+        }
+
+        getPosition() {
+            return this.point;
+        }
+
+        getMap() {
+            return this.map;
+        }
+    };
 
     constructor(...args) {
         super(...args);
@@ -29,81 +88,66 @@ export default class MapView extends Injector {
             const callback = 'mapcb' + Math.random().toString().substr(2);
             $('body').append($(`<script src="http://api.map.baidu.com/api?v=2.0&ak=${scope.apiKey}&callback=${callback}"></script>`));
             window[callback] = () => {
+                let items = [];
+                angular.copy(scope.items, items);
                 this._showItems(scope, mapId);
-                scope.$watch('items', () => {
-                    this._showItems(scope, mapId);
+                scope.$watch('items', (newVal) => {
+                    if (!angular.equals(items, newVal)) {
+                        this._showItems(scope, mapId);
+                    }
                 });
             };
         } else {
             this.$injected.$timeout(() => {
+                let items = [];
+                angular.copy(scope.items, items);
                 this._showItems(scope, mapId);
-                scope.$watch('items', () => {
-                    this._showItems(scope, mapId);
+                scope.$watch('items', (newVal) => {
+                    if (!angular.equals(items, newVal)) {
+                        this._showItems(scope, mapId);
+                    }
                 });
             });
         }
     }
 
-    _showItems(scope, mapId) {
-        if (!scope.items || scope.items.length) return;
+    async _showItems(scope, mapId) {
+        if (!scope.items || !scope.items.length) return;
         if (scope.map) {
             scope.markerClusterer.clearMarkers();
             scope.markers = [];
             scope.items.forEach(i => {
-                scope.markers.push(new MapViewOverlay(i, scope.onClick, scope.onMouseover));
+                scope.markers.push(new MapView.MapViewOverlay(i, scope.onClick, scope.onMouseover));
             });
-            scope.markerClusterer = new window.BMapLib.MarkerClusterer(map, {markers: scope.markers});
+            scope.markerClusterer = new window.BMapLib.MarkerClusterer(scope.map, {markers: scope.markers});
         } else {
             let map = scope.map = new window.BMap.Map(mapId);
-            map.enableScollWheelZoom(true);
+            map.enableScrollWheelZoom(true);
             map.addControl(new window.BMap.NavigationControl({
                 anchor: window.BMAP_ANCHOR_TOP_RIGHT,
                 type  : window.BMAP_NAVIGATION_CONTROL_ZOOM,
             }));
-            map.centerAndZoom(scope.center || '北京', 5);
+
+            if (!scope.center) {
+                await new Promise(resolve => {
+                    new window.BMap.LocalCity().get(result => {
+                        scope.center = result.name;
+                        resolve();
+                    });
+                });
+            }
+            await new Promise(resolve => {
+                new window.BMap.Geocoder().getPoint(scope.center, point => {
+                    map.centerAndZoom(point, 5);
+                    resolve();
+                });
+            });
 
             scope.markers = [];
             scope.items.forEach(i => {
-                scope.markers.push(new MapViewOverlay(i, scope.onClick, scope.onMouseover));
+                scope.markers.push(new MapView.MapViewOverlay(i, scope.onClick, scope.onMouseover));
             });
             scope.markerClusterer = new window.BMapLib.MarkerClusterer(map, {markers: scope.markers});
         }
-    }
-}
-
-class MapViewOverlay extends window.BMap.Overlay {
-    constructor(object, onmouseover, onclick) {
-        this.point       = new window.BMap.Point(object.lng, object.lat);
-        this.label       = object.label;
-        this.data        = object;
-        this.onmouseover = onmouseover;
-        this.onclick     = onclick;
-    }
-
-    initialize(map) {
-        this.map = map;
-        var div  = this.div = $(`<div>${this.label}</div>`);
-        div.css({
-            
-        });
-        div.onmouseout(this.onmouseover).onclick(this.onclick);
-        map.getPanes().markerPane.appendChild(div);
-        return div;
-    }
-
-    draw() {
-        var pixel = this.map.pointToOverlayPixel(this.point);
-        this.div.css({
-            left: pixel.x - 10,
-            top : pixel.y - 30,
-        });
-    }
-
-    getPosition() {
-        return this.point;
-    }
-
-    getMap() {
-        return this.map;
     }
 }
