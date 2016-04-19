225c6c71d0fc68ebd2fbe015d848ef3d00297c5f
dchuangincar <dchuang@incardata.com.cn>
2016-04-19 13:34:57 +0800

    添加后台派单

--- src/main/java/com/autobon/platform/controller/admin/OrderController.java
+++ src/main/java/com/autobon/platform/controller/admin/OrderController.java
@@ -1,5 +1,6 @@
 package com.autobon.platform.controller.admin;
 
+import com.autobon.getui.PushService;
 import com.autobon.order.entity.Comment;
 import com.autobon.order.entity.DetailedOrder;
 import com.autobon.order.entity.Order;
@@ -13,13 +14,17 @@ import com.autobon.shared.JsonPage;
 import com.autobon.shared.VerifyCode;
 import com.autobon.staff.entity.Staff;
 import com.autobon.technician.entity.TechStat;
+import com.autobon.technician.entity.Technician;
 import com.autobon.technician.service.TechStatService;
+import com.autobon.technician.service.TechnicianService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.im4java.core.ConvertCmd;
 import org.im4java.core.IMOperation;
 import org.im4java.process.Pipe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
@@ -27,13 +32,16 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 /**
@@ -47,9 +55,11 @@ public class OrderController {
     @Value("${com.autobon.uploadPath}") String uploadPath;
     @Autowired OrderService orderService;
     @Autowired CommentService commentService;
+    @Autowired TechnicianService technicianService;
     @Autowired TechStatService techStatService;
     @Autowired DetailedOrderService detailedOrderService;
     @Autowired ApplicationEventPublisher publisher;
+    @Autowired @Qualifier("PushServiceA") PushService pushServiceA;
 
     @RequestMapping(method = RequestMethod.GET)
     public JsonMessage search(
@@ -75,7 +85,8 @@ public class OrderController {
             try {
                 Order.Status s = Order.Status.valueOf(orderStatus);
                 statusCode = s.getStatusCode();
-            } catch (Exception e) {}
+            } catch (Exception e) {
+            }
         }
 
         return new JsonMessage(true, "", "",
@@ -91,9 +102,10 @@ public class OrderController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public JsonMessage createOrder(HttpServletRequest request,
-            @RequestParam("orderType")   int orderType,
-            @RequestParam("orderTime")   String orderTime,
+    public JsonMessage createOrder(
+            HttpServletRequest request,
+            @RequestParam("orderType") int orderType,
+            @RequestParam("orderTime") String orderTime,
             @RequestParam("positionLon") String positionLon,
             @RequestParam("positionLat") String positionLat,
             @RequestParam("contactPhone") String contactPhone,
@@ -123,7 +135,7 @@ public class OrderController {
 
     @RequestMapping(value = "/photo", method = RequestMethod.POST)
     public JsonMessage uploadPhoto(HttpServletRequest request,
-            @RequestParam("file") MultipartFile file) throws Exception {
+                                   @RequestParam("file") MultipartFile file) throws Exception {
         if (file == null || file.isEmpty()) return new JsonMessage(false, "NO_UPLOAD_FILE", "没有上传文件");
 
         String path = "/uploads/order";
@@ -133,7 +145,7 @@ public class OrderController {
         String originalName = file.getOriginalFilename();
         String extension = originalName.substring(originalName.lastIndexOf('.')).toLowerCase();
         String filename = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
-                            + VerifyCode.generateVerifyCode(6) + extension;
+                + VerifyCode.generateVerifyCode(6) + extension;
 
         InputStream in;
         if (file == null || file.isEmpty()) return new JsonMessage(false, "没有选择上传文件");
@@ -151,8 +163,46 @@ public class OrderController {
         return new JsonMessage(true, "", "", path + "/" + filename);
     }
 
+    @RequestMapping(value = "/assign", method = RequestMethod.POST)
+    public JsonMessage assign(
+            @RequestParam("orderId") int orderId,
+            @RequestParam("techId") int techId) throws IOException {
+        Order order = orderService.get(orderId);
+        if (order == null) return new JsonMessage(false, "NO_SUCH_ORDER", "订单不存在");
+        if (order.getStatus() != Order.Status.NEWLY_CREATED) return new JsonMessage(false, "UNASSIGNABLE_ORDER", "订单已有人接单或已取消");
+
+        Technician tech = technicianService.get(techId);
+        if (tech == null) return new JsonMessage(false, "NO_SUCH_TECH", "技师不存在");
+        if (tech.getStatus() != Technician.Status.VERIFIED) return new JsonMessage(false, "TECH_NOT_VERIFIED", "技师未认证,不可接单");
+        if (tech.getSkill() == null || !Arrays.stream(tech.getSkill().split(",")).anyMatch(i -> i.equals("" + order.getOrderType())))
+            return new JsonMessage(false, "TECH_SKILL_NOT_SUFFICIANT", tech.getName() + "没有此订单类型的技能认证");
+
+        order.setMainTechId(techId);
+        order.setStatus(Order.Status.TAKEN_UP);
+        orderService.save(order);
+
+        // 更新订单总数
+        TechStat stat = techStatService.getByTechId(tech.getId());
+        if (stat == null) {
+            stat = new TechStat();
+            stat.setTechId(tech.getId());
+        }
+        stat.setTotalOrders(stat.getTotalOrders() + 1);
+        techStatService.save(stat);
+
+        // 推送派单消息
+        String msgTitle = "你收到派单消息";
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("action", "ASSIGN_ORDER");
+        map.put("order", order);
+        map.put("title", msgTitle);
+        boolean result = pushServiceA.pushToSingle(tech.getPushId(), msgTitle, new ObjectMapper().writeValueAsString(map), 72*3600);
+        if (!result) log.error("订单: " + order.getOrderNum() + "的派单消息发送失败");
+        return new JsonMessage(true, "", "", order);
+    }
+
     @RequestMapping(value = "/comment", method = RequestMethod.POST)
-    public JsonMessage comment(HttpServletRequest request,
+    public JsonMessage comment(
             @RequestParam("orderId") int orderId,
             @RequestParam("star") int star,
             @RequestParam("advice") String advice,
