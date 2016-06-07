0d78c64aef1a1fc2a51f4293123e97eafa6a5400
775473732 <775473732@qq.com>
2016-06-07 15:40:23 +0800

    添加订单时绑定技师的测试

--- src/test/java/com/autobon/test/coop/OrderControllerTest.java
+++ src/test/java/com/autobon/test/coop/OrderControllerTest.java
@@ -112,4 +112,71 @@ public class OrderControllerTest {
                 .andExpect(jsonPath("$.result", is(true)));
     }
 
+    //测试订单带指定技师
+    @Test   //主技师未通过审核
+    public void createOrderAndAppoint1()throws Exception {
+        mockMvcS.perform(post("/api/mobile/coop/order/createOrderAndAppoint")
+                .param("photo","photo")
+                .param("remark","订单注释")
+                .param("orderTime","2016-02-10 09:23")
+                .param("orderType","1")
+                .param("mainTechId","2")
+                .cookie(new Cookie("autoken", token)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.result", is(false)));
+    }
+
+    @Test   //日期时间不正确
+    public void createOrderAndAppoint2()throws Exception {
+        mockMvcS.perform(post("/api/mobile/coop/order/createOrderAndAppoint")
+                .param("photo","photo")
+                .param("remark","订单注释")
+                .param("orderTime","2016-02-1009:23")
+                .param("orderType","1")
+                .param("mainTechId","1")
+                .cookie(new Cookie("autoken", token)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.result", is(false)));
+    }
+
+    @Test   //主技师无此技能
+    public void createOrderAndAppoint3()throws Exception {
+        mockMvcS.perform(post("/api/mobile/coop/order/createOrderAndAppoint")
+                .param("photo","photo")
+                .param("remark","订单注释")
+                .param("orderTime","2016-02-10 09:23")
+                .param("orderType","2")
+                .param("mainTechId","1")
+                .cookie(new Cookie("autoken", token)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.result", is(false)));
+    }
+
+    @Test   //商户未通过验证
+    public void createOrderAndAppoint4()throws Exception {
+        mockMvcS.perform(post("/api/mobile/coop/order/createOrderAndAppoint")
+                .param("photo","photo")
+                .param("remark","订单注释")
+                .param("orderTime","2016-02-10 09:23")
+                .param("orderType","1")
+                .param("mainTechId","1")
+                .cookie(new Cookie("autoken", token)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.result", is(false)));
+    }
+
+    @Test   //订单创建并指定技师
+    public void createOrderAndAppoint5()throws Exception {
+        mockMvcS.perform(post("/api/mobile/coop/order/createOrderAndAppoint")
+                .param("photo","photo")
+                .param("remark","订单注释")
+                .param("orderTime","2016-02-10 09:23")
+                .param("orderType","1")
+                .param("mainTechId","1")
+                .cookie(new Cookie("autoken", token)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.result", is(true)));
+    }
+
+
 }
