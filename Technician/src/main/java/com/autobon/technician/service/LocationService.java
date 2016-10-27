cec9fa729c4045b4dcfcd5dd5a8fbf39cbad8982
wangh2015 <379954552@qq.com>
2016-10-27 17:57:32 +0800

    增加技师技能   定位状态

--- Technician/src/main/java/com/autobon/technician/service/LocationService.java
+++ Technician/src/main/java/com/autobon/technician/service/LocationService.java
@@ -1,7 +1,9 @@
 package com.autobon.technician.service;
 
 import com.autobon.technician.entity.Location;
+import com.autobon.technician.entity.LocationStatus;
 import com.autobon.technician.repository.LocationRepository;
+import com.autobon.technician.repository.LocationStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,7 +19,21 @@ public class LocationService {
     @Autowired
     private LocationRepository repository;
 
+    @Autowired
+    private LocationStatusRepository locationStatusRepository;
+
     public Location save(Location location) {
+        LocationStatus locationStatus = locationStatusRepository.findByTechId(location.getTechId());
+        if(locationStatus != null){
+            LocationStatus locationStatus1 = new LocationStatus(location);
+            locationStatus1.setId(locationStatus.getId());
+            locationStatus1.setStatus(locationStatus.getStatus());
+            locationStatusRepository.save(locationStatus1);
+        }else{
+            LocationStatus locationStatus1 = new LocationStatus(location);
+            locationStatus1.setStatus(1);
+            locationStatusRepository.save(locationStatus1);
+        }
         return repository.save(location);
     }
 
--- src/main/java/com/autobon/platform/controller/admin/TechnicianController.java
+++ src/main/java/com/autobon/platform/controller/admin/TechnicianController.java
@@ -45,51 +45,98 @@ public class TechnicianController {
         return new JsonMessage(true, "", "", detailedTechnicianService.get(techId));
     }
 
+    /**
+     * 技师申请认证
+     * @param techId 技师ID
+     * @param phone 手机号码
+     * @param name 技师姓名
+     * @param gender 技师性别
+     * @param idNo 身份证号
+     * @param idPhoto 手持身份证图片
+     * @param bank 银行
+     * @param bankAddress 银行开户行
+     * @param bankCardNo 银行卡号
+     * @param skill 技能  逗号分隔
+     * @param filmLevel  隔热膜星级
+     * @param carCoverLevel   车衣星级
+     * @param colorModifyLevel   改色星级
+     * @param beautyLevel 美容星级
+     * @param resume 个人简介
+     * @return
+     */
     @RequestMapping(value = "/{techId:\\d+}", method = RequestMethod.POST)
     public JsonMessage update(@PathVariable("techId") int techId,
-            @RequestParam(value = "phone") String phone,
-            @RequestParam(value = "name") String name,
-            @RequestParam(value = "gender") String gender,
-            @RequestParam(value = "idNo") String idNo,
-            @RequestParam(value = "idPhoto") String idPhoto,
-            @RequestParam(value = "bank") String bank,
-            @RequestParam(value = "bankAddress") String bankAddress,
-            @RequestParam(value = "bankCardNo") String bankCardNo,
-            @RequestParam(value = "skill") String skill) {
+                              @RequestParam(value = "phone") String phone,
+                              @RequestParam(value = "name") String name,
+                              @RequestParam(value = "gender") String gender,
+                              @RequestParam(value = "idNo") String idNo,
+                              @RequestParam(value = "idPhoto") String idPhoto,
+                              @RequestParam(value = "bank") String bank,
+                              @RequestParam(value = "bankAddress") String bankAddress,
+                              @RequestParam(value = "bankCardNo") String bankCardNo,
+                              @RequestParam(value = "skill") String skill,
+                              @RequestParam(value = "filmLevel", required = false) int filmLevel,
+                              @RequestParam(value = "carCoverLevel", required = false) int carCoverLevel,
+                              @RequestParam(value = "colorModifyLevel", required = false) int colorModifyLevel,
+                              @RequestParam(value = "beautyLevel", required = false) int beautyLevel,
+                              @RequestParam(value = "resume", required = false) String resume,
+                              @RequestParam(value = "reference", required = false) String reference)throws Exception{
 
         if (!Pattern.matches("^\\d{11}$", phone)) {
             return  new JsonMessage(false,"ILLEGAL_PARAM","手机号格式错误");
-        } else if (technicianService.getByPhone(phone) != null) {
+        }
+
+        if (technicianService.getByPhone(phone) != null) {
             return new JsonMessage(false,"OCCUPIED_ID","手机号已被注册");
         }
 
-        idNo = idNo.toUpperCase();
-        if (!Pattern.matches("^(\\d{15})|(\\d{17}[0-9X])$", idNo))
+        if (!Pattern.matches("^(\\d{15})|(\\d{17}[0-9X])$",  idNo.toUpperCase())) {
             return new JsonMessage(false, "ILLEGAL_PARAM", "身份证号码有误");
-        if (!Pattern.matches("^\\d+(,\\d+)*$", skill))
+        }
+
+        if (!Pattern.matches("^\\d+(,\\d+)*$", skill)) {
             return new JsonMessage(false, "ILLEGAL_PARAM", "参数skill格式错误, 请填写技能编号并用分号分隔");
+        }
 
         Technician technician = technicianService.get(techId);
         technician.setPhone(phone);
         technician.setName(name);
         technician.setGender(gender);
-        technician.setIdNo(idNo);
+        technician.setIdNo(idNo.toUpperCase());
         technician.setIdPhoto(idPhoto);
         technician.setBank(bank);
         technician.setBankAddress(bankAddress);
         technician.setBankCardNo(bankCardNo);
         technician.setSkill(skill);
+        technician.setFilmLevel(filmLevel);
+        technician.setCarCoverLevel(carCoverLevel);
+        technician.setColorModifyLevel(colorModifyLevel);
+        technician.setBeautyLevel(beautyLevel);
+        technician.setResume(resume);
+        technician.setReference(reference);
         technicianService.save(technician);
         return new JsonMessage(true,"","",technician);
     }
 
+
+    /**
+     * 后台认证
+     * @param techId
+     * @param verified
+     * @param verifyMsg
+     * @return
+     * @throws IOException
+     */
     @RequestMapping(value = "/verify/{techId:\\d+}", method = RequestMethod.POST)
-    public JsonMessage verify(
-            @PathVariable("techId") int techId,
-            @RequestParam("verified") boolean verified,
-            @RequestParam(value = "verifyMsg", defaultValue = "") String verifyMsg) throws IOException {
+    public JsonMessage verify(@PathVariable("techId") int techId,
+                              @RequestParam("verified") boolean verified,
+                              @RequestParam(value = "verifyMsg", defaultValue = "") String verifyMsg) throws IOException {
+
+
         Technician tech = technicianService.get(techId);
-        if (tech == null) return new JsonMessage(false, "ILLEGAL_PARAM", "没有这个技师");
+        if (tech == null){
+            return new JsonMessage(false, "ILLEGAL_PARAM", "没有这个技师");
+        }
 
         if (verified) {
             tech.setVerifyMsg(null);
@@ -119,8 +166,8 @@ public class TechnicianController {
 
     @RequestMapping(value = "/maptrack/{techId:\\d+}", method = RequestMethod.GET)
     public JsonMessage getTechnicianTrack(@PathVariable("techId") int techId,
-            @RequestParam(value = "page", defaultValue = "1") int page,
-            @RequestParam(value = "pageSize", defaultValue = "20") int pageSize) {
+                                          @RequestParam(value = "page", defaultValue = "1") int page,
+                                          @RequestParam(value = "pageSize", defaultValue = "20") int pageSize) {
         return new JsonMessage(true, "", "", new JsonPage<>(locationService.findByTechId(techId, page, pageSize)));
     }
 }
