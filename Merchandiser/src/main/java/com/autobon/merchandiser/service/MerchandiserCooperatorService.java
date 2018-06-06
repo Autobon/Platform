2e82f41554c60177e567888c53d476963c6ab74a
wangh2015 <hwang2@incardata.com.cn>
2018-06-06 18:03:58 +0800

    跟单员

--- Merchandiser/src/main/java/com/autobon/merchandiser/service/MerchandiserCooperatorService.java
+++ Merchandiser/src/main/java/com/autobon/merchandiser/service/MerchandiserCooperatorService.java
@@ -1,8 +1,12 @@
 package com.autobon.merchandiser.service;
 
 import com.autobon.merchandiser.repository.MerchandiserCooperatorRepository;
+import com.autobon.merchandiser.vo.MerchandiserCooperatorShow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wh on 2018/6/5.
@@ -14,6 +18,17 @@ public class MerchandiserCooperatorService {
     MerchandiserCooperatorRepository merchandiserCooperatorRepository;
 
 
+
+    public List<MerchandiserCooperatorShow> findByMerchandiserId(int merchandiserId){
+
+        List<MerchandiserCooperatorShow> merchandiserCooperatorShows = new ArrayList<>();
+        List<Object[]> objects = merchandiserCooperatorRepository.find(merchandiserId);
+        for(Object[] o: objects){
+            merchandiserCooperatorShows.add(new MerchandiserCooperatorShow(o));
+        }
+
+        return merchandiserCooperatorShows;
+    }
 
 
 }
