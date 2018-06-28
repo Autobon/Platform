c388585b594baa326b2d41951cb390ebaf1e7256
wangh2015 <hwang2@incardata.com.cn>
2018-06-28 10:04:40 +0800

    查询所有菜单及API类别

--- src/main/java/com/autobon/platform/controller/admin/RoleController.java
+++ src/main/java/com/autobon/platform/controller/admin/RoleController.java
@@ -1,12 +1,19 @@
 package com.autobon.platform.controller.admin;
 
 import com.autobon.shared.JsonMessage;
+import com.autobon.staff.entity.FunctionCategory;
+import com.autobon.staff.entity.Menu;
 import com.autobon.staff.service.FunctionCategoryService;
+import com.autobon.staff.service.MenuService;
+import com.autobon.staff.vo.MenuShow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wh on 2018/6/22.
@@ -18,11 +25,28 @@ public class RoleController {
 
     @Autowired
     FunctionCategoryService functionCategoryService;
+    @Autowired
+    MenuService menuService;
 
     @RequestMapping(value = "/function/category/menu/{menuId}", method = RequestMethod.GET)
-    public JsonMessage logout(@PathVariable("menuId") int menuId) {
+    public JsonMessage menu(@PathVariable("menuId") int menuId) {
 
         return new JsonMessage(true, "", "", functionCategoryService.findByMenuId(menuId));
     }
+
+    @RequestMapping(value = "/function/category/menu", method = RequestMethod.GET)
+    public JsonMessage menu1() {
+
+        List<MenuShow> menuShowList  = new ArrayList<>();
+
+        List<Menu> menus = menuService.findMenus();
+        for(Menu menu : menus){
+            List<FunctionCategory> functionCategories = functionCategoryService.findByMenuId(menu.getId());
+            menuShowList.add(new MenuShow(menu, functionCategories));
+        }
+
+        return new JsonMessage(true, "", "",menuShowList);
+    }
+
 
 }
