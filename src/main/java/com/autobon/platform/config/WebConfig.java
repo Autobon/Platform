0cf61dba27564c655fc3f24a5eb24218bba3173c
liubo <1332479746@qq.com>
2018-09-10 09:31:31 +0800

    .

--- src/main/java/com/autobon/platform/config/WebConfig.java
+++ src/main/java/com/autobon/platform/config/WebConfig.java
@@ -3,12 +3,15 @@ package com.autobon.platform.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.ServletContext;
+import java.util.List;
 
 /**
  * Created by dave on 16/2/18.
@@ -26,6 +29,32 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         resolver.setResolveLazily(true);
         return resolver;
     }
+
+//    @Bean
+//    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
+//        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+//        //设置日期格式
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd");
+//        objectMapper.setDateFormat(smt);
+//        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+//        //设置中文编码格式
+//        List<MediaType> list = new ArrayList<MediaType>();
+//        list.add(MediaType.APPLICATION_JSON_UTF8);
+//        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
+//        return mappingJackson2HttpMessageConverter;
+//    }
+
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        converters.add(getMappingJackson2HttpMessageConverter());
+//    }
+//
+//    private MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
+//        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+//        return messageConverter;
+//    }
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
